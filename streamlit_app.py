8297cc412fad5f37961b32c4866a1767b0593f9d
rashmiverma-dev <rashmi.verma1992@gmail.com>
2026-07-28 15:24:30 +0530

    Display smoothie fruit response as a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,4 +62,5 @@ if selected_value:
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 
-st.json(smoothiefroot_response.json())
+df = st.json(smoothiefroot_response.json())
+st.dataframe(data=df,use_container_width=True)
